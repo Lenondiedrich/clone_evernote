764876a15cdc20a72449b6bbf9183e3375e7b210
Lenon Diedrich <lenon.diedrich@gmail.com>
2021-06-03 15:36:19 -0300

    Get note route created and authorization configs

--- production/javascript_note_api/app/routes/notes.js
+++ production/javascript_note_api/app/routes/notes.js
@@ -19,7 +19,11 @@ router.get('/:id', withAuth, async(req, res) => {
     try {
         const { id } = req.params;
         let note = await Note.findById(id);
-
+        if(isOwner(req.user, note)) {
+            res.json(note);
+        } else {
+            res.status(403).json({error: 'Permission denied'});
+        }
     } catch (error) {
         res.status(500).json({error: 'Problem to get a note'});
     }
