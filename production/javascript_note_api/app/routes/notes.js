70c157d7e21c90132961255e32ddafc9f2169bcf
Lenon Diedrich <lenon.diedrich@gmail.com>
2021-06-03 15:30:06 -0300

    Note creation route

---
+++ production/javascript_note_api/app/routes/notes.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+const Note = require('../models/note');
+const withAuth = require('../middlewares/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    const { title, body } = req.body;
+    
+    try {
+        let note = new Note({title: title, body: body, author: req.user._id});
+        await note.save();
+        res.status(200).json(note);
+    } catch(error) {
+        res.status(500).json({error: 'Problem to create a new note'});
+    }
+});
+
+router.get('/:id', withAuth, async(req, res) => {
+    try {
+        const { id } = req.params;
+        let note = await Note.findById(id);
+
+    } catch (error) {
+        res.status(500).json({error: 'Problem to get a note'});
+    }
+});
+
+const isOwner = (user, note) => {
+    if(JSON.stringify(user._id) == JSON.stringify(note.author._id)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = router;
