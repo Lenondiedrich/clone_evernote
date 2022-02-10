78e73d9adfea586d2a575717d9de0ce884a1f109
Lenon <lenon.diedrich@gmail.com>
2022-02-10 20:12:36 -0300

    First deploy

--- production/javascript_note_api/config/database.js
+++ production/javascript_note_api/config/database.js
@@ -1,9 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/javascriptNote', {
+require("dotenv").config();
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose
+  .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
-}).then(() => console.log('Connection successful'))
-.catch((err) => console.log(err));
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connection successful"))
+  .catch((err) => console.log(err));
